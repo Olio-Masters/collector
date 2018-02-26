a5e360af82fac534653128c4ba72729e2b0556d4
C0mmunist1 <36222715+C0mmunist1@users.noreply.github.com>
2018-02-26 11:41:07 +0200

    Update Beacons.java

--- src/beacons/Beacons.java
+++ src/beacons/Beacons.java
@@ -9,37 +9,56 @@ import lejos.hardware.sensor.SensorMode;
 
 public class Beacons extends Thread {
 	/**
-	 * Class Beacon is an extension of Thread. 
-	 * The point is to search beacons constantly utilizing run() method.
+	 * Class Beacon is an extension of Thread. The point is to search beacons
+	 * constantly utilizing run() method.
 	 * 
 	 */
-	
-
 
 	private final EV3IRSensor irsensor1;
-	private SensorMode position;
-	private String draw = "";
-	private float[] seekSample = new float[position.sampleSize()];
-	
-	public Beacons(){
+	private String draw;
+	private float[] seekSample;
+	private final SampleProvider position;
+	private int beaconInfoH;
+	private int beaconInfoD;
+
+	public Beacons() {
 		this.irsensor1 = new EV3IRSensor(SensorPort.S4);
+		this.draw = "";
+		this.seekSample = new float[position.sampleSize()];
 		this.position = irsensor1.getSeekMode();
+		this.beaconInfoD = 0;
+		this.beaconInfoH = 0;
+
 	}
+
 	
+	/**
+	 * Don't call this method. Instead call method "start()"
+	 * 
+	 * This method periodically primes the various variables in
+	 * this class. To use the variables, call the other methods in this class.
+	 */
 	@Override
-	public void run() {
+	public void run(Button.ESCAPE.isUp) {
 		try {
+			while(Button.ESCAPE.isUp) {
 			this.position = irsensor1.getSeekMode();
-			irsensor1.fetchSample(seekSample, 0);
-			this.draw = "" + seekSample[0];
 			
-			Thread.sleep(10);
+			position.fetchSample(sample, 0);
+
+			beaconInfoH = (int) sample[2]; // sample for direction
+			beaconInfoD = (int) sample[3]; // sample for distance
+			
+			this.draw = "" + seekSample[0];		//Add samples to string
+			Thread.sleep(20);
+			}
 		} catch(Exception e) {
 		}
 	}
-	
+
 	/**
 	 * SensorMode method getSensorMode()
+	 * 
 	 * @return returns the position of a beacon as a SensorMode variable.
 	 */
 	public SensorMode getSensorMode() {
@@ -47,7 +66,28 @@ public class Beacons extends Thread {
 	}
 	
 	/**
-	 * toString() 
+	 * This method returns the distance of the beacon in relation to the infraredsensor
+	 * in the robot.
+	 * 
+	 * @return returns int variable of the distance of the beacon.
+	 */
+	public int distance() {
+		return beaconInfoD;
+	}
+	
+	/**
+	 * This method returns the angle of the beacon in relation to the infraredsensor
+	 * in the robot.
+	 * 
+	 * @return returns int variable of the angle of the beacon.
+	 */
+	public int angle() {
+		return BeaconInfoH;
+	}
+
+	/**
+	 * toString()
+	 * 
 	 * @return returns a String of the SensorMode variables to be printed.
 	 */
 	@Override
